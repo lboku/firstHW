0c1fe9afa76920e6aa1b23271419def9e5718367
lboku <129886726+lboku@users.noreply.github.com>
2023-10-02 16:04:33 +0400

    0-ეტაპი; 1 კვირა

---
+++ firstHW.js
@@ -0,0 +1,60 @@
+//1)დაწერე პროგრამა რომელიც კონსოლში გამოიტანს "Hello World" -ს.
+
+console.log("hello world")
+
+
+//2)შექმენით ცვლადი და მიანიჭეთ მას რიცხვი. დაბეჭდეთ ცვლადის მნიშვნელობა.
+
+let num=4;
+console.log(num);
+//3)დაწერეთ პროგრამა ორი რიცხვის შესაკრებად და შედეგის გამოსატანად.
+
+let num1 =10;
+let num2 =5;
+let result = num1 + num2;
+console.log(result);
+
+
+//4)დაწერეთ პროგრამა, რომელიც გამოაკლებს ორ რიცხვს ერთმანეთს და აჩვენებს შედეგს.
+let number1=10;
+let number2=5;
+let result2=number1 - number2;
+console.log(result2);
+
+//5)დაწერეთ პროგრამა ორი რიცხვის გასამრავლებლად და შედეგის გამოსატანად.
+let numberA=10;
+let numberB=5;
+let result3=numberA * numberB;
+console.log(result3);
+
+
+//6)დაწერეთ პროგრამა ორი რიცხვის გასაყოფად და შედეგის გამოსატანად.
+let numberC=10;
+let numberD=5;
+let result4=numberC / numberD;
+console.log(result4);
+
+//7)დაწერეთ პროგრამა ორი რიცხვის გაყოფის ნაშთის გამოსათვლელად.
+
+let numberE=7;
+let numberF=3;
+let result5=numberE / numberF;
+console.log(result5);
+//8)შექმენით ცვლადი და მიანიჭეთ მას სტრიქონი. ამობეჭდეთ სტრიქონის სიგრძე.
+let text = ("my name is lasha");
+console.log(text.length);
+
+//9)დაწერეთ პროგრამა ორი სტრიქონის შესაერთებლად და შედეგის ჩვენების მიზნით.
+let fName = ("my name is lasha");
+let sName = ("bokuchava");
+let fullName = fName +" "+ sName;
+console.log(fullName);
+
+//10)დაწერეთ პროგრამა სტრიქონის დიდ ასოებზე გადასაყვანად.
+let clOn = ("caps lock on");
+console.log(clOn.toUpperCase());
+
+//11)დაწერეთ პროგრამა სტრიქონის მცირე ასოზე გადასაყვანად.
+
+let clOff = ("CAPS LOCK OFF");
+console.log(clOff.toLowerCase());
